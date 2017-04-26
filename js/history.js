85a6c32972c2315112bc04769811197464720842
Matt <m_dittmann@yahoo.com>
2017-04-25 21:47:54 -0700

    add click function to handle links and update history

--- js/history.js
+++ js/history.js
@@ -4,7 +4,16 @@ $(function () {
 		$('#content').load(url + ' #container').hide().fadeIn('slow');
 	}
 
-	
+	//CLICK FUNCTION TO HANDLE LINKS AND UPDATE HISTORY
+	$('nav a').on('click', function(e) {
+		e.preventDefault();
+		var href = this.href;
+		var $this = $(this);
+		$('a').removeClass('current');
+		$this.addClass('current');
+		loadContent(href);
+		history.pushState('', $this.text, href);
+	});
 
 
 });
